3ba4a56cca88f20b2d5162958bf4f8eeeb47a87f
Ben Kovitz <bkovitz@indiana.edu>
2024-06-30 18:55:25 -0700

    "egg-laying" model
    Givers increase the carrying capacity of nearby cells. Each taker lays
    an egg in every cell in the neighborhood. Carrying capcity determines
    how many eggs survive into adulthood.

--- init-alt2/collective-gain.cc
+++ init-alt2/collective-gain.cc
@@ -1,7 +1,9 @@
 /*
  * collective-gain.cc -- Givers & Takers simulation
  *
- * Very simple: givers add to number of offspring of neighboring takers.
+ * Very simple: Givers increase the carrying capacity of nearby cells. The
+ * carrying capacity of a cell is the probability that an organism there will
+ * survive to adulthood.
  */
 
 //#define BOOST_TEST_DYN_LINK  // enable or compile with -D for unit tests
@@ -15,6 +17,7 @@
 #include <map>
 #include <ranges>
 #include <functional>
+#include <algorithm>
 #include <getopt.h>
 
 #include "random.h"
@@ -30,16 +33,27 @@ RandomUniformReal random_angle = RandomUniformReal(0.0, 2.0 * M_PI);
 double initial_g = 0.01;  // g of all organisms in zeroth generation
 int num_gens = 10000;       // number of generations to run
 double base_fecundity = 1.0;
-int world_xsize = 20;
-int world_ysize = 20;
+double base_carrying_capacity = 0.09;
+  // the carrying capacity of a cell before any givers add to it
+double cc_delta = 0.00; //0.20;
+  // how much one giver increases a cell's carrying capacity
+int world_xsize = 30;
+int world_ysize = 30;
 double acorn_stdev = 2.0;  // std deviation of child's distance from parent
+int giving_radius = 2;  // how far away givers give
+int laying_radius = 2;  // how far away organisms lay eggs
+double mutation_stdev = 0.05;  // std deviation of mutation of g
+
+bool show_givers = false;
+bool show_carrying_capacity = false;
+bool show_next_organisms = false;
 
 struct Coord;
 
 std::ostream& operator<<(std::ostream& os, const Coord& c);
 
 struct Coord {
-  const int x, y; 
+  int x, y; 
 
   bool operator==(const Coord& that) const {
     return x == that.x && y == that.y;
@@ -107,22 +121,59 @@ std::ostream& operator<<(std::ostream& os, const Organism& o) {
   return os;
 }
 
+struct Parent {
+  const Coord& coord;
+  const Organism& organism;
+};
+
+struct Egg {
+  Coord egg_location;
+  const Coord* parent_location;
+  const Organism* parent;
+
+  //Egg(const Coord& egg_loc, const Coord& parent_loc, const Organism& parent_)
+  //: egg_location(egg_loc), parent_location(&parent_loc), parent(parent_) { }
+
+  void swap(Egg& that) noexcept {
+      using std::swap;
+      swap(egg_location, that.egg_location);
+      swap(parent_location, that.parent_location);
+      swap(parent, that.parent);
+  }
+};
+
+std::ostream& operator<<(std::ostream& os, const Egg& egg) {
+  return os << egg.egg_location << ' ' << *egg.parent_location << ' '
+            << *egg.parent;
+}
+
+namespace std {
+  template <>
+  void swap(Egg& a, Egg& b) noexcept {
+    a.swap(b);
+  }
+}
+
+typedef std::unordered_map<Coord, Egg> EggMap;
+
 class World {
 public:
   const int xsize, ysize;
   std::unordered_map<Coord, Organism> organisms;       // current population
   std::unordered_map<Coord, Organism> next_organisms;  // next generation's population
+  std::vector<double> carrying_capacity;
 
-  const int giving_radius = 4;
   int children_attempted = 0;
   int children_produced = 0;
   int num_givers = 0;
   
-  RandomNormal random_mutation_delta = RandomNormal(0.0, 0.01);
+  RandomNormal random_mutation_delta = RandomNormal(0.0, mutation_stdev);
 
-  World(int x, int y) : xsize(x), ysize(y), organisms(), next_organisms() { }
+  World(int x, int y) : xsize(x), ysize(y), organisms(), next_organisms(),
+    carrying_capacity() { initialize_carrying_capacity(); }
   World(int x, int y, std::unordered_map<Coord, Organism>&& orgs)
-  : xsize(x), ysize(y), organisms(std::move(orgs)), next_organisms() { }
+  : xsize(x), ysize(y), organisms(std::move(orgs)), next_organisms(),
+    carrying_capacity() { initialize_carrying_capacity(); }
 
   static World make_world(int x, int y, int num_initial_organisms = 20) {
 //    auto place_random_initial_organism = [&]() {
@@ -138,7 +189,7 @@ public:
       if (m.size() >= num_initial_organisms)
         break;
     }
-      
+
     //cout << "World made. size=" << m.size() << endl;
     return World(x, y, std::move(m));
   }
@@ -146,9 +197,10 @@ public:
   void run_one_generation() {
     children_attempted = 0;
     children_produced = 0;
-    num_givers = 0;
     develop_into_givers();
     cout << "organisms:" << endl;
+    for (auto& p : shuffled_organisms())
+      cout << p.get().first << ' ' << p.get().second << endl;
     print_as_grid(organisms);
     givers_spread_fecundity();
     print_fecundity();
@@ -157,7 +209,40 @@ public:
     print();
   }
 
+  /*
+   * Runs a generation in which givers increase the carrying capacity of
+   * neighboring cells.
+   */
+  void run_one_generation_cc() {
+    develop_into_givers();
+    if (show_givers) {
+      cout << "organisms (showing givers):\n";
+      print_as_grid(organisms);
+    }
+    reset_carrying_capacity();
+    givers_increase_carrying_capacity();
+    if (show_carrying_capacity)
+      print_carrying_capacities();
+    takers_reproduce_by_eggs();
+    std::swap(organisms, next_organisms);
+    if (show_next_organisms)
+      print();
+  }
+
+  void reset_carrying_capacity() {
+    std::fill(
+      carrying_capacity.begin(),
+      carrying_capacity.end(),
+      base_carrying_capacity
+    );
+  }
+
+  void initialize_carrying_capacity() {
+    carrying_capacity.resize(xsize * ysize, base_carrying_capacity);
+  }
+
   void develop_into_givers() {
+    num_givers = 0;
     for (Organism& o : std::views::values(organisms)) {
       o.develop_into_giver_or_taker();
       if (o.is_giver)
@@ -200,6 +285,26 @@ public:
       pair.get().second.fecundity = 0.0;
   }
 
+  void givers_increase_carrying_capacity() {
+    for (auto& pair : givers()) {
+      const Coord& c = pair.get().first;
+      //Organism& o = pair.second;
+      for (Coord& d : c.delta_square(giving_radius)) {
+        Coord target_cell_coord = add_coords(c, d);
+        carrying_capacity[index2d(target_cell_coord)] += cc_delta;
+      }
+    }
+  }
+
+  int index2d(int x, int y) {
+    return y * ysize + x;
+  }
+
+  int index2d(const Coord& c) {
+    //return c.y * ysize + c.x;
+    return index2d(c.x, c.y);
+  }
+
   std::vector<std::reference_wrapper<std::pair<const Coord, Organism>>>
   givers() {
     std::vector<std::reference_wrapper<std::pair<const Coord, Organism>>>
@@ -220,6 +325,88 @@ public:
         reproduce(o, coord);
     }
   }
+
+  void takers_reproduce_by_eggs() {
+    next_organisms.clear();
+    for (auto& egg : std::views::values(lay_eggs())) {
+      auto r = unit_interval_random();
+      auto cc = carrying_capacity[index2d(egg.egg_location)];
+      /*
+      cout << "loc = " << egg.egg_location
+           << ", r = " << r
+           << ", cc = " << cc << endl;
+      */
+      if (r <= cc) {
+        //cout << "  ^survived\n";
+        next_organisms.emplace(egg.egg_location, mutated(egg.parent->g));
+      }
+    }
+  }
+
+  // TODO rm
+  std::vector<std::reference_wrapper<std::pair<const Coord, Organism>>>
+  shuffled_organisms() {
+    std::vector<std::reference_wrapper<std::pair<const Coord, Organism>>>
+    result(organisms.begin(), organisms.end());
+
+    std::shuffle(result.begin(), result.end(), randoms::rng);
+    return result;
+  }
+
+  EggMap lay_eggs() {
+    std::vector<Egg> eggs;
+    for (auto& pair : organisms) {
+      const Coord& c = pair.first;
+      Organism& o = pair.second;
+      if (!o.is_giver) {  // if taker
+        for (const Coord& d : c.delta_square(laying_radius)) {
+          Coord egg_location = add_coords(c, d);
+          eggs.emplace_back(Egg{egg_location, &c, &o});
+        }
+      }
+    }
+    std::shuffle(eggs.begin(), eggs.end(), randoms::rng);
+
+    EggMap result;
+    int maxsize = ysize * xsize;
+    for (const Egg& egg : eggs) {
+      if (result.find(egg.egg_location) == result.end()) {
+        result.emplace(egg.egg_location, egg);
+        if (result.size() >= maxsize)
+          break;
+      }
+    }
+    cout << "number of eggs: " << result.size() << endl;
+    return result;
+  }
+
+  /*
+  typedef std::map<std::reference_wrapper<const Coord>,
+          std::reference_wrapper<Organism>> EggMap;
+
+  // NEXT Make attempted_reproductions(): a list of
+  //   (organism, where to lay an egg)
+  EggMap lay_eggs() {
+    EggMap eggs{};
+
+    for (auto& pair : organisms) {
+      const Coord& c = pair.first;
+      Organism& o = pair.second;
+      if (!o.is_giver) {  // if taker
+        for (const Coord& d : c.delta_square(laying_radius)) {
+          Coord target_coord = add_coords(c, d);
+          if (eggs.find(target_coord) == eggs.end())
+            eggs.emplace(target_coord, o);
+        }
+      }
+    }
+    
+
+  }
+  */
+  // Better idea: make a vector of eggs, shuffle it, and reduce that to
+  // a map of eggs.
+  // Better idea: write the calling code first.
 
   void reproduce(const Organism& o, const Coord& coord) {
     //double num_children = random_poisson(o.fecundity);
@@ -325,9 +512,9 @@ public:
   }
 
   void print_as_grid(const std::unordered_map<Coord, Organism>& organisms) {
-    for (int i = 0; i < xsize; i++) {
-      for (int j = 0; j < ysize; j++) {
-        Coord c{i, j};
+    for (int i = 0; i < ysize; i++) {
+      for (int j = 0; j < xsize; j++) {
+        Coord c{j, i};
         if (organisms.count(c) == 0)
           cout << "   - ";
         else
@@ -340,9 +527,9 @@ public:
   void print_fecundity() {
     cout << "fecundities:" << endl;
     //cout << "num_givers: " << num_givers << endl;
-    for (int i = 0; i < xsize; i++) {
-      for (int j = 0; j < ysize; j++) {
-        Coord c{i, j};
+    for (int i = 0; i < ysize; i++) {
+      for (int j = 0; j < xsize; j++) {
+        Coord c{j, i};
         if (organisms.count(c) == 0)
           cout << "   - ";
         else if (organisms.at(c).is_giver)
@@ -354,6 +541,16 @@ public:
       cout << endl;
     }
     cout << organisms.size() << endl;
+  }
+
+  void print_carrying_capacities() {
+    cout << "carrying capacities:" << endl;
+    for (int i = 0; i < ysize; i++) {
+      for (int j = 0; j < xsize; j++)
+        cout << std::fixed << std::setw(4) << std::setprecision(2)
+             << carrying_capacity[index2d(j, i)] << ' ';
+      cout << '\n';
+    }
   }
 };
 
@@ -415,9 +612,36 @@ void run_experiment() {
   }
 }
 
+void run_experiment_cc() {
+  World w = World::make_world(world_xsize, world_ysize);
+  cout << "gen 0" << endl;
+  w.print();
+  for (int gen = 1; gen < num_gens + 1; gen++) {
+    cout << endl << "gen" << gen << endl;
+    w.run_one_generation_cc();
+    //w.print();
+  }
+}
+
+void run_special() {
+  World w = World::make_world(world_xsize, world_ysize);
+  w.print();
+  auto eggs = w.lay_eggs();
+  for (auto& egg : std::views::values(eggs)) {
+    cout << egg << endl;
+    if (unit_interval_random() < w.carrying_capacity[w.index2d(egg.egg_location)])
+      w.next_organisms.emplace(
+        egg.egg_location,
+        w.mutated(egg.parent->g)
+      );
+  }
+  w.print_as_grid(w.next_organisms);
+  cout << w.next_organisms.size() << endl;
+}
+
 void print_command_line_options()
 {
-  static std::string lc = " \\\n";  // line continuation
+  static const std::string lc = " \\\n";  // line continuation
   cout << "--initial_g=" << initial_g << lc;
   cout << "--num_gens=" << num_gens << lc;
   cout << "--xsize=" << world_xsize << lc;
@@ -425,6 +649,12 @@ void print_command_line_options()
   cout << "--base_fecundity=" << base_fecundity << lc;
   cout << "--seed=" << randoms::seed << lc;
   cout << "--acorn_stdev=" << acorn_stdev << lc;
+  cout << "--base_carrying_capacity=" << base_carrying_capacity << lc;
+  // TODO show_givers, etc.
+  cout << "--giving_radius=" << giving_radius << lc;
+  cout << "--laying_radius=" << giving_radius << endl;
+  cout << "--cc_delta=" << cc_delta << endl;
+  cout << "--mutation_stdev=" << mutation_stdev << endl;
 }
 
 /*
@@ -445,6 +675,14 @@ int parse_command_line_options(int argc, char** argv)
     { "base_fecundity", required_argument, 0, 0},
     { "seed", required_argument, 0, 0},
     { "acorn_stdev", required_argument, 0, 0},
+    { "base_carrying_capacity", required_argument, 0, 0},
+    { "show_givers", no_argument, 0, 0},
+    { "show_carrying_capacity", no_argument, 0, 0},
+    { "show_next_organisms", no_argument, 0, 0},
+    { "giving_radius", required_argument, 0, 0},
+    { "laying_radius", required_argument, 0, 0},
+    { "cc_delta", required_argument, 0, 0},
+    { "mutation_stdev", required_argument, 0, 0},
     { NULL, 0, 0, 0 }
   };
 
@@ -474,6 +712,30 @@ int parse_command_line_options(int argc, char** argv)
         break;
       case 6:
         acorn_stdev = atof(optarg);
+        break;
+      case 7:
+        base_carrying_capacity = atof(optarg);
+        break;
+      case 8:
+        show_givers = true;
+        break;
+      case 9:
+        show_carrying_capacity = true;
+        break;
+      case 10:
+        show_next_organisms = true;
+        break;
+      case 11:
+        giving_radius = atoi(optarg);
+        break;
+      case 12:
+        laying_radius = atoi(optarg);
+        break;
+      case 13:
+        cc_delta = atof(optarg);
+        break;
+      case 14:
+        mutation_stdev = atof(optarg);
         break;
       default:
         cerr << "Internal error\n";
@@ -509,6 +771,8 @@ int main(int argc, char** argv) {
   /*
   set_up_experiment1();
   */
-  run_experiment();
+  //run_experiment();
+  //run_special();
+  run_experiment_cc();
   return 0;
 }
