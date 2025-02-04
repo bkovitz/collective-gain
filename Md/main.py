980cc977170006b976af4fe0735c31444e852f5c
Zoltan A. Kocsis <11808286+zaklogician@users.noreply.github.com>
2025-02-04 22:54:42 +0000

    World initialization
    Signed-off-by: Zoltan A. Kocsis <11808286+zaklogician@users.noreply.github.com>

--- Md/main.py
+++ Md/main.py
@@ -3,35 +3,61 @@ import numpy as np
 
 class Experiment(NamedTuple):
     seed: int
-    initial_value: np.int64
-    min_step: np.int64
-    max_step: np.int64
+    generations: int
+    initial_organism_count: int
+    initial_carrier_count: int
 
-class IterationState(NamedTuple):
-    value: np.int64
+    giving_radius: np.float64
+    giver_development_probability: np.float64
+
+    child_placement_radius: np.float64
+    number_of_children: int
+    base_survival_probability: np.float64
+    giver_survival_bonus: np.float64 
+
+
+# the main array containing all individuals in the world
+World = np.ndarray # shape=(N,5), dtype=np.float64
+XCOORD: int = 0
+YCOORD: int = 1
+CARRIER: int = 2
+ALIVE: int = 3
+GIVER: int = 4
+
+
+def initialize_world(the_experiment: Experiment, the_rng: np.random.Generator) -> World:
+    total_population = the_experiment.initial_organism_count
+    the_world: World = np.zeros((total_population, 5), dtype=np.float64)
+    the_world[:,[XCOORD,YCOORD]] = the_rng.random((total_population, 2))
+    the_world[0:the_experiment.initial_carrier_count,CARRIER] = 1
+    return the_world
 
 class Simulation:
-    def __init__(self, theExperiment: Experiment) -> None:
-        self.theExperiment = theExperiment
-        self.rng = np.random.default_rng(theExperiment.seed)
-        self.history: List[IterationState] = [IterationState(theExperiment.initial_value)]
+    def __init__(self, the_experiment: Experiment) -> None:
+        self.experiment = the_experiment
+        self.rng = np.random.default_rng(the_experiment.seed)
+        self.history: List[World] = [initialize_world(the_experiment, self.rng)]
 
     def step(self) -> None:
-        last_value = self.history[-1].value
-        step = self.rng.integers(
-            self.theExperiment.min_step,
-            self.theExperiment.max_step + 1,
-            dtype=np.int64
-        )
-        new_value = last_value + step
-        self.history.append(IterationState(new_value))
+        self.history.append(self.history[-1])
 
-    def run(self, steps: int) -> None:
-        for _ in range(steps):
+    def run(self) -> None:
+        for _ in range(self.experiment.generations):
             self.step()
 
-e: Experiment = Experiment(seed=31337, initial_value=0, min_step=0-5, max_step=5)
+e: Experiment = Experiment(
+    seed=31337,
+    generations=1,
+    initial_organism_count=10,
+    initial_carrier_count=1,
+    giving_radius=np.float64(0.00),
+    giver_development_probability=np.float64(0.00),
+    child_placement_radius=np.float64(1.00),
+    number_of_children=3,
+    base_survival_probability=np.float64(0.1),
+    giver_survival_bonus=np.float64(0.1)
+  )
 s: Simulation = Simulation(e)
 
-s.run(10)
-print(s.history)
+s.run()
+print(s.history[0])
